79bad8dfbf695aca540204e8662b05738780e5eb
pip <jrphiri@gmail.com>
2019-11-08 09:07:02 +0200

    Layout as HOC

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,5 @@
-import Layout from '../components/MyLayout';
-export default function About() {
-  return (
-    <Layout>
-      <p>This is the about page</p>
-    </Layout>
-  );
-}
+import withLayout from '../components/MyLayout';
+
+const Page = () => <p>This is the about page</p>;
+
+export default withLayout(Page);
