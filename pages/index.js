9113d5a53e2c6b76872d3bb49baf95f17e4d5eb2
pip <jrphiri@gmail.com>
2019-11-08 11:11:56 +0200

    Added styles with styled-jsx

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,36 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
 
-const PostLink = props => (
+function getPosts() {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'learn-nextjs', title: 'Learning Next.js is awesome' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+  ];
+}
+
+const PostLink = ({ post }) => (
   <li>
-    <Link href="/p/[id]" as={`/p/${props.id}`}>
-      <a>{props.id}</a>
+    <Link href="/p/[id]" as={`/p/${post.id}`}>
+      <a>{post.title}</a>
     </Link>
+    <style jsx>
+      {`
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}
+    </style>
   </li>
 );
 
@@ -14,10 +39,22 @@ export default function Blog() {
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        <PostLink id="hello-next.js" />
-        <PostLink id="learn-next.js" />
-        <PostLink id="deploy-next.js" />
+        {getPosts().map(post => (
+          <PostLink key={post.id} post={post} />
+        ))}
       </ul>
+      <style jsx>
+        {`
+        h1,
+        a{
+          font-family: 'Arial';}
+        }
+
+        ul {
+          padding: 0;
+        }
+        `}
+      </style>
     </Layout>
   );
 }
